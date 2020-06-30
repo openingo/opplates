3e51f125848a425de3ec327675e0b4d2b0549585
Qicz <qiczzhu@gmail.com>
2020-06-30 17:20:37 +0800

    add genenetor and readme

---
+++ MyBatisPlusGenerator/OrclGenerator.java
@@ -0,0 +1,123 @@
+package generator;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 自动生成 Mybatis Entity和Mapper, xml
+ *
+ * @author Qicz
+ */
+public class OrclGenerator {
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        String projectName = "";
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + projectName +"/src/main/java");
+        gc.setAuthor("Qicz");
+        gc.setActiveRecord(true);
+        gc.setOpen(false);
+        gc.setFileOverride(true); // 重写生成时覆盖旧文件
+        gc.setEntityName("%sDO");// Entity后缀
+        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:oracle:thin:@192.168.1.38:1521:ORCL");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("oracle.jdbc.OracleDriver");
+        dsc.setUsername("ORCL");
+        dsc.setPassword("123456");
+
+        //dsc.setDbType(DbType.ORACLE);
+        //dsc.setTypeConvert(new OracleTypeConvert());
+        mpg.setDataSource(dsc);
+
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName("module");
+        pc.setParent("org.openingo");
+
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        String templatePath = "mapper.xml.vm";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + projectName +"/src/main/resources/mapper.orcl/" + tableInfo.getMapperName() + StringPool.DOT_XML;
+            }
+        });
+
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        templateConfig.setEntity("entity.java");
+        templateConfig.setMapper("mapper.java");
+        //templateConfig.setService("");
+        //templateConfig.setServiceImpl("");
+        templateConfig.setController("");
+
+        templateConfig.setXml(null);
+
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setTablePrefix("T_");
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        //strategy.setSuperEntityClass(MpModelExt.class);
+        strategy.setEntityLombokModel(true);
+        //strategy.setRestControllerStyle(true);
+        // 公共父类
+        //strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
+        // 写于父类中的公共字段
+        //strategy.setSuperEntityColumns("id");
+       // strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+       // strategy.setControllerMappingHyphenStyle(true);
+        //strategy.setTablePrefix(pc.getModuleName() + "_");
+
+        //strategy.setEntityTableFieldAnnotationEnable(true);
+
+        //TableFill createTimeFill = new TableFill("create_time", FieldFill.INSERT);
+        //TableFill updateTimeFill = new TableFill("update_time", FieldFill.INSERT_UPDATE);
+
+        //strategy.setTableFillList(Arrays.asList(createTimeFill, updateTimeFill));
+
+        mpg.setStrategy(strategy);
+
+        //mpg.setTemplateEngine(new VelocityTemplateEngine());
+
+
+        mpg.execute();
+    }
+}
--- README.md
+++ README.md
@@ -13,3 +13,122 @@
 
   - logback
 
+- MyBatisPlusGenerator依赖
+
+  > 建议在test中生成即可。
+
+  - properties
+
+    ```properties
+    <properties>
+          <java.version>1.8</java.version>
+          <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+          <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
+          <maven.compiler.source>${java.version}</maven.compiler.source>
+          <maven.compiler.target>${java.version}</maven.compiler.target>
+          <lombok.version>1.18.8</lombok.version>
+          <springboot.version>2.1.8.RELEASE</springboot.version>
+          <druid.version>1.1.21</druid.version>
+          <ojdbc.version>11.2.0.4.0-atlassian-hosted</ojdbc.version>
+          <mysql.sersion>8.0.17</mysql.sersion>
+          <springboot.mybatis.version>2.1.0</springboot.mybatis.version>
+          <mybatis-plus.version>3.3.2</mybatis-plus.version>
+          <mybatis-x.version>1.0</mybatis-x.version>
+          <jdkits.version>1.0.1</jdkits.version>
+          <postgresql.version>42.2.14</postgresql.version>
+          <velocity.version>2.2</velocity.version>
+      </properties>
+    ```
+
+  - 必须依赖
+
+    ```xml
+    <!-- 开启lombok时 -->
+    <dependency>
+        <groupId>org.projectlombok</groupId>
+        <artifactId>lombok</artifactId>
+        <version>${lombok.version}</version>
+    </dependency>
+    
+    <dependency>
+        <groupId>org.mybatis.spring.boot</groupId>
+        <artifactId>mybatis-spring-boot-starter</artifactId>
+        <version>${springboot.mybatis.version}</version>
+    </dependency>
+    
+    <!-- mybatis plus -->
+    <dependency>
+        <groupId>com.baomidou</groupId>
+        <artifactId>mybatis-plus-boot-starter</artifactId>
+        <version>${mybatis-plus.version}</version>
+    </dependency>
+    
+    <!-- mybatis plus generator with test envoriment -->
+    <dependency>
+        <groupId>com.baomidou</groupId>
+        <artifactId>mybatis-plus-generator</artifactId>
+        <version>${mybatis-plus.version}</version>
+        <scope>test</scope>
+    </dependency>
+    <dependency>
+        <groupId>org.apache.velocity</groupId>
+        <artifactId>velocity-engine-core</artifactId>
+        <version>${velocity.version}</version>
+        <scope>test</scope>
+    </dependency>
+    
+    <!-- test -->
+    <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-test</artifactId>
+        <scope>test</scope>
+    </dependency>
+    ```
+
+  - MySQL
+
+    ```xml
+    <dependency>
+        <groupId>mysql</groupId>
+        <artifactId>mysql-connector-java</artifactId>
+        <version>${mysql.sersion}</version>
+    </dependency>
+    <dependency>
+        <groupId>org.openingo.kits</groupId>
+        <artifactId>mybatis-x</artifactId>
+        <version>${mybatis-x.version}</version>
+    </dependency>
+    ```
+
+  - Oracle
+
+    ```xml
+    <dependency>
+        <groupId>com.oracle</groupId>
+        <artifactId>ojdbc6</artifactId>
+        <version>${ojdbc.version}</version>
+    </dependency>
+    ```
+
+  - PostgreSQL
+
+    ```xml
+    <dependency>
+        <groupId>org.postgresql</groupId>
+        <artifactId>postgresql</artifactId>
+        <version>${postgresql.version}</version>
+    </dependency>
+    ```
+
+##### 问题：
+
+##### Oracle
+
+> 表名及字段都需要用大写，mp gererator出来的 date字段为LocalDateTime，但按照此类型写入数据时，类型错误，需要调整为java.util.Date
+
+##### PostgreSQL
+
+> 表名及字段必须用小写，不然会出现字段找不到的情况。
+>
+> 在生成的entity中没有@TableField注解
+
